7db550475fd7192ecd6850fcf1612bbbb746c96c
Eugene <inbox@null.page>
2025-05-25 17:34:40 +0200

    tweak animation

--- theme-src/animations/home.js
+++ theme-src/animations/home.js
@@ -10,42 +10,72 @@ if (container) {
 
 gsap.registerPlugin(MotionPathPlugin)
 
-const totalDuration = 2
+const totalDuration = 8
 
-function animatePath(boxSelector, pathSelector, options, duration = 2) {
-    const path = `${pathSelector}  > g > :nth-child(3)`
+function trackBorderSelector(id) {
+    return `#${id} > g > path:not(:nth-child(3))`
+}
+
+function animatePath(tl, boxSelector, pathSelector, options = {}) {
+    const path = `${pathSelector} > g > :nth-child(3)`
     document.querySelector(path).style.display = 'none'
-    let tl = gsap.timeline({ repeat: -1 })
+
+    tl.addLabel('label')
     tl.fromTo(boxSelector, {
         opacity: 0
     }, {
         opacity: 1,
         duration: 0.5,
+        delay: options.delay ?? null,
     })
-        .to(boxSelector, {
-            yoyo: false,
-            duration,
-            ease: "power1.inOut",
-            motionPath: {
-                path: path,
-                align: path,
-                autoRotate: true,
-                start: 1,
-                end: 0,
-                ...options,
-            },
-        }, "<")
-        .to(boxSelector, {
-            opacity: 0,
-            duration: 0.5,
-        }, "-=0.5")
-        .set({}, {}, totalDuration)
-
+    tl.to(boxSelector, {
+        yoyo: false,
+        duration: options.duration ?? 2,
+        ease: "power1.inOut",
+        motionPath: {
+            path: path,
+            align: path,
+            autoRotate: true,
+            start: options.start ?? 0,
+            end: options.end ?? 1,
+        },
+    }, "label")
+    tl.to(boxSelector, {
+        opacity: 0,
+        duration: 0.5,
+    }, "-=0.5")
 }
 
-animatePath("#box", "#path", { start: 0, end: 1 }, 1.45)
-animatePath("#box2", "#path2")
-animatePath("#box3", "#path3", { start: 0, end: 1 }, 1)
+let tl = gsap.timeline({ repeat: -1 })
 
-const tl = gsap.timeline({ repeat: -1 })
-tl.fromTo('#flyingparticles', { x: -250 }, { x: 200, duration: 0.5, delay: 0.9 }).set({}, {}, totalDuration)
+// auth path
+tl.addLabel('startAuth')
+animatePath(tl, "#box", "#path", { duration: 1 })
+animatePath(tl, "#box3", "#path3", { duration: 1 })
+animatePath(tl, "#box3", "#path3", { start: 1, end: 0, duration: 1 })
+animatePath(tl, "#box", "#path", { start: 1, end: 0, duration: 1 })
+tl.addLabel('endAuth')
+
+// data path
+tl.addLabel('startData')
+tl.addLabel('data1')
+animatePath(tl, "#box2", "#path2", { start: 1, end: 0 })
+tl.addLabel('data2')
+animatePath(tl, "#box2", "#path2", { start: 1, end: 0 })
+tl.addLabel('endData')
+
+tl
+    .fromTo('#flyingparticles', { x: -250 }, { x: 200, duration: 0.5 }, 'data1+=1')
+    .fromTo('#flyingparticles', { x: -250 }, { x: 200, duration: 0.5 }, 'data2+=1')
+
+const trackOnColor = '#777'
+const trackOffColor = '#444'
+tl.to(trackBorderSelector('path'), { stroke: trackOnColor, duration: 0.5 }, 'startAuth')
+tl.to(trackBorderSelector('path3'), { stroke: trackOnColor, duration: 0.5 }, 'startAuth')
+tl.to(trackBorderSelector('path'), { stroke: trackOffColor, duration: 0.5 }, 'endAuth')
+tl.to(trackBorderSelector('path3'), { stroke: trackOffColor, duration: 0.5 }, 'endAuth')
+
+tl.to(trackBorderSelector('path2'), { stroke: trackOnColor, duration: 0.5 }, 'startData')
+tl.to(trackBorderSelector('path2'), { stroke: trackOffColor, duration: 0.5 }, 'endData')
+
+// tl.set({}, {}, totalDuration)
